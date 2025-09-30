ae72499d800175b07be083f6d6238fb2ea2c18a4
ElCzar <cesarandresolarte@gmail.com>
2025-09-30 14:42:04 -0500

    feat: add dependency and configuration for eureka registration

--- microservicio_perfil/src/main/java/edu/microservicio/microservicio_perfil/MicroservicioPerfilApplication.java
+++ microservicio_perfil/src/main/java/edu/microservicio/microservicio_perfil/MicroservicioPerfilApplication.java
@@ -2,8 +2,10 @@ package edu.microservicio.microservicio_perfil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicioPerfilApplication {
 
 	public static void main(String[] args) {
